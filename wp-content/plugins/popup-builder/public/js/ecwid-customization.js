04489d101da8c05cbe2ab95721d2f875dfe6e7f5
sunberrysolar <34402076+sunberrysolar@users.noreply.github.com>
2025-11-18 10:34:28 +0100

    Update ecwid-customization.js test4

--- wp-content/plugins/popup-builder/public/js/ecwid-customization.js
+++ wp-content/plugins/popup-builder/public/js/ecwid-customization.js
@@ -12,7 +12,7 @@ window.ec.config.storefrontUrls = window.ec.config.storefrontUrls || {};
 (function injectImmediateCSS() {
   const style = document.createElement('style');
   style.textContent = `
-    /* Masquer le prix unitaire et total ligne dès le début test3*/
+    /* Masquer le prix unitaire et total ligne dès le début test4*/
     .ec-cart__item-price,
     .ec-cart__total-price,
     .ec-cart-item__price-inner {
